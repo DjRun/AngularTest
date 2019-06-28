055a99110335c27eaa236ae1acd722f98f216711
Djrun <aghazadajeyran@gmail.com>
2019-06-28 15:03:08 +0400

    hello

---
+++ my-dream-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  input = 'Write anything for see it';
+}
